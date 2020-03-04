428fe21d7f267f4cfa96cec96a5e7b82f00918cb
Joseph Wright <joseph.wright@morningstar2.co.uk>
2020-03-04 15:18:17 +0000

    Initial l3build set-up

---
+++ build.lua
@@ -0,0 +1,11 @@
+#!/usr/bin/env texlua
+
+-- Identify the bundle and module
+bundle = ""
+module = "mathtools"
+
+-- Self-contained sources
+unpackfiles  = {"*.dtx"}
+
+-- Release a TDS-style zip
+packtdszip  = true
